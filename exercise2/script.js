44f63c968a9c8423eacfe2348e9fdc6990097416
EmiTorres93 <emilianat1992@gmail.com>
2023-06-22 13:17:59 -0300

    push exercise 2

--- exercise2/script.js
+++ exercise2/script.js
@@ -2,8 +2,10 @@ const person = [];
 const button = document.getElementById("button");
 const contenedor = document.getElementById("contenedor");
 const show = document.getElementById("show");
+const showGeneration = document.getElementById("showGeneration");
 let inputSelect;
 let generation;
+let show2;
 
 let inputAll = document.querySelectorAll(".input");
 inputAll.forEach((input) => {
@@ -24,17 +26,27 @@ const cargar = () => {
   const datos = document.createElement("div");
   datos.innerHTML =
     /*HTML*/
-    `<h5>Tus datos ingresados son: ${person} </h5> 
-     <p>La generación a la que perteneces es: `;
+    `<h5>Tus datos ingresados son: ${person}</h5>
+    `;
   contenedor.appendChild(datos);
   show.className = "";
-  generation = Number(person[4]);
+  generation = parseInt(person[4]);
   console.log(generation);
-  if (1993 < generation <= 2010) {
+  if (generation > 1993 && generation <= 2010) {
+    show2 = "Generación Z";
+    showGeneration.innerText = `La generación a la que perteneces es: ${show2}`;
     console.log("Generación Z");
-  } else if (1981 < generation <= 1994) {
-    console.log("Generación X 'millenials'");
-  } else if (1969 < generation <= 1980) {
-    console.log("Generación ..");
+  } else if (generation > 1981 && generation <= 1994) {
+    show2 = "Generación Y 'millenials'";
+    showGeneration.innerText = `La generación a la que perteneces es: ${show2}`;
+  } else if (generation > 1969 && generation <= 1980) {
+    show2 = "Generación X'";
+    showGeneration.innerText = `La generación a la que perteneces es: ${show2}`;
+  } else if (generation > 1949 && generation <= 1968) {
+    show2 = "Generación Baby Boom";
+    showGeneration.innerText = `La generación a la que perteneces es: ${show2}`;
+  } else {
+    show2 = "Silent Generation";
+    showGeneration.innerText = `La generación a la que perteneces es: ${show2}`;
   }
 };
